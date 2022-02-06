9415ba6ceab6c3f25273b322f91cca7837255083
mrbenc88 <mrbenc88@students.cs.ubc.ca>
2022-02-06 15:47:47 -0800

    add keep alive script to maintain db

--- server.js
+++ server.js
@@ -65,24 +65,50 @@
 
 // Pass in our defined port variable so our server can accept a parameter from the environment and what port to listen to
 // Log to console to confirm it is running.
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-let cors = require('cors');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+let cors = require("cors");
 const port = process.env.PORT || 5000;
 
-require('dotenv/config');
+function startKeepAlive() {
+  setInterval(function () {
+    var options = {
+      host: "backendforweb3.herokuapp.com",
+      port: 80,
+      path: "/",
+    };
+    http
+      .get(options, function (res) {
+        res.on("data", function (chunk) {
+          try {
+            // optional logging... disable after it's working
+            console.log("HEROKU RESPONSE: " + chunk);
+          } catch (err) {
+            console.log(err.message);
+          }
+        });
+      })
+      .on("error", function (err) {
+        console.log("Error: " + err.message);
+      });
+  }, 20 * 60 * 1000); // load every 20 minutes
+}
+
+startKeepAlive();
+
+require("dotenv/config");
 
 app.use(cors());
 app.use(bodyParser.json());
 
-const dropRoute = require('./routes/drop');
+const dropRoute = require("./routes/drop");
 
-app.use('/drop', dropRoute);
+app.use("/drop", dropRoute);
 
 mongoose.connect(process.env.DB_CONNECTION, () => {
-    console.log("connected to db");
-})
+  console.log("connected to db");
+});
 
 app.listen(port);
